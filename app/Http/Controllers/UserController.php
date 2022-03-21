26f83dde30f7ee204a86901972b2153a7c28d97c
Younas Mahmood <55877886+Younas22@users.noreply.github.com>
2022-03-21 22:29:11 +0500

    o

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -134,10 +134,7 @@ class UserController extends Controller
     {
         $database = $this->_getDatabaseConnection();
         $config = config('grocerycrud');
-
         $crud = new GroceryCrud($config, $database);
-
-
         $crud->setTable('orders');
         $crud->setSubject('orders', 'orders');
         $crud->columns(['invoice_id','user_id','note_box','other_address','pickup_date','delivery_date','amount','order_date']);
@@ -147,42 +144,8 @@ class UserController extends Controller
         $crud->displayAs('user_id','Name');
         $crud->where(['orders.user_id' => session('logAdmin')->user_id]);
         $crud->unsetReadFields(['card_type', 'card_holder_name', 'first_day','second_day','therd_day','fourth_day']);
-
-
-    // $crud->setFieldUpload('image', 'public/assets/images/customer', env('APP_URL').'/public/assets/images/customer');
-    // $crud->callbackBeforeDelete(function ($stateParameters) {
-    // $check = DB::table('child_products')->where('cp_id',$stateParameters->primaryKeyValue)->get();
-    // unlink(public_path().'/assets/images/customer' .  $check[0]->image);
-    // return $stateParameters;
-    // });
-    // $crud->callbackColumn('image', function ($value, $row) {
-    // $val = env('APP_URL').'/public/assets/images/customer/'.$value;
-    // $img = "<div class='thumbnail span1'><center><img src='$val' style='border: 1px solid #ddd; border-radius: 4px; width: 100px;' /></center></div>";
-    // return($img);
-    // });
-
-        // $crud->uniqueFields(['email']);
-        // $crud->callbackBeforeInsert(function ($stateParameters) {
-        //     $stateParameters->data['created_at'] = Carbon::now();
-        //     $stateParameters->data['updated_at'] = Carbon::now();
-        //     $password = Hash::make($stateParameters->data['password']);
-        //     $stateParameters->data['password'] = $password;
-        //     return $stateParameters;
-        // });
-
-        // $crud->callbackBeforeUpdate(function ($stateParameters) {
-        //     $check = DB::table('users')->where('user_id',$stateParameters->primaryKeyValue)->get();
-        //     if($check[0]->password != $stateParameters->data['password']){
-        //         $password = Hash::make($stateParameters->data['password']);
-        //         $stateParameters->data['password'] = $password;
-        //     }
-        //     return $stateParameters;
-        // });
-
-
         $crud->setRead();
         $crud->setSkin('Flexigrid');
-        // $crud->unsetFields(['coupon_code','coupon_val','coupon_status','hide_class']);
         $output = $crud->render();
         if ($output->isJSONResponse) {
             return response($output->output, 200)
@@ -193,7 +156,6 @@ class UserController extends Controller
         $css_files = $output->css_files;
         $js_files = $output->js_files;
         $output = $output->output;
-
         return view('backend.grocery.grocery_example', [
             'output' => $output,
             'css_files' => $css_files,
@@ -202,7 +164,6 @@ class UserController extends Controller
     }
 
     private function _getDatabaseConnection() {
-
         return [
             'adapter' => [
                 'driver' => 'Pdo_Mysql',
