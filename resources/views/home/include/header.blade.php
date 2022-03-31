b04dce90a9255f37741b8d6cd0c6d35597defc53
Younas Mahmood <hm.younas22@gmail.com>
2022-03-31 12:52:13 +0500

    design updated

--- resources/views/home/include/header.blade.php
+++ resources/views/home/include/header.blade.php
@@ -24,7 +24,7 @@
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapse-mobile" aria-expanded="false" aria-controls="collapse-mobile">
         <i class="bi bi-list h1"></i>
       </button>
-      <ul class="navbar-nav ml-auto align-items-center d-none d-lg-flex">
+      <ul class="navbar-nav ml-auto align-items-center">
         <li class="nav-item active">
           <a class="nav-link text-uppercase" href="{{url('/')}}">Home</a>
         </li>
@@ -65,19 +65,6 @@
             </li>
             <?php } ?>
 
-<!--    <li class="nav-item">
-          <form class="form-inline mb-0">
-            <div class="input-group">
-              <div class="input-group-prepend ">
-                <span class="input-group-text border-0 bg-transparent searchicon">
-                  <i data-feather="search"></i>
-                </span>
-              </div>
-              <input type="text" class="form-control border-0 search-navbar" placeholder="Search..." aria-label="search" aria-describedby="basic-addon1">
-            </div>
-          </form>
-        </li> -->
-
       </ul>
     </nav>
     <a class="btn btn-primary" style="min-width: 0 !important;" href="{{url('/login')}}"><i data-feather="layers"></i>Sign In</a>
@@ -88,33 +75,55 @@
     <div class="container-fluid">
       <div class="row">
         <div class="col-12">
-          <ul class="navbar-nav">
-            <li class="nav-item">
-              <a class="nav-link text-uppercase" href="/">Home</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link text-uppercase" href="blog.php">Blog</a>
-            </li>
 
-            <?php  if (session()->has('logAdmin') && session('logAdmin')->role == 'customer') { ?>
-            <li class="nav-item">
-              <a class="nav-link text-uppercase" href="{{url('/dashboard')}}">Dashboard</a>
+          <ul class="navbar-nav">
+
+        <li class="nav-item active">
+          <a class="nav-link text-uppercase" href="{{url('/')}}">Home</a>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link text-uppercase" href="#">Laundry Services</a>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link text-uppercase" href="#">Linen Subscription</a>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link text-uppercase" href="#">Commercial linen</a>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link text-uppercase" href="#about_us">About Us</a>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link text-uppercase" href="#">Contact Us</a>
+        </li>
+
+
+        <li class="nav-item">
+          <a class="nav-link text-uppercase" href="#">FAQ</a>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link text-uppercase" href="{{url('/blog')}}">Blog</a>
+        </li>
+         <?php  if (session()->has('logAdmin') && session('logAdmin')->role == 'customer') { ?>
+              <li class="nav-item">
+                <a class="nav-link text-uppercase" href="{{url('/dashboard')}}">Dashboard</a>
             </li>
-            <?php } ?>
-<!-- 
-            <li class="nav-item">
-              <form class="form-inline mb-0">
-                <div class="input-group">
-                  <div class="input-group-prepend ">
-                    <span class="input-group-text border-0 bg-transparent searchicon">
-                      <i data-feather="search"></i>
-                    </span>
-                  </div>
-                  <input type="text" class="form-control border-0 search-navbar" placeholder="Search..." aria-label="search" aria-describedby="basic-addon1">
-                </div>
-              </form>
-            </li> -->
+          <?php } ?>
+
+
+        <li class="nav-item">
+            <a class="btn btn-primary px-4" style="min-width: 0 !important;" href="{{url('/login')}}"><i data-feather="layers" style="width: 20px; height: 20px;" class="mr-2"></i> Sign In</a>
+        </li>
+
+
           </ul>
+
         </div>
       </div>
     </div>
