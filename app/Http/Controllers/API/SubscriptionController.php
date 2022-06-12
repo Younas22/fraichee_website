65cb5b956ca6aafe000d19fee43b8c9a78182085
Younas Mahmood <55877886+Younas22@users.noreply.github.com>
2022-06-12 20:08:36 +0500

    i

--- app/Http/Controllers/API/SubscriptionController.php
+++ app/Http/Controllers/API/SubscriptionController.php
@@ -6,29 +6,28 @@ use App\Http\Controllers\API\BaseController as BaseController;
 // use App\Models\User;
 use Illuminate\Support\Facades\Auth;
 use Validator;
+use Illuminate\Support\Facades\DB;
    
 class SubscriptionController extends BaseController
 {
-	    public function subscription_products(Request $request)
+	    public function subscription_services(Request $request)
     {
-        // $validator = Validator::make($request->all(), [
-        //     'name' => 'required',
-        //     'email' => 'required|email',
-        //     'password' => 'required',
-        //     'c_password' => 'required|same:password',
-        // ]);
-   
-        // if($validator->fails()){
-        //     return $this->sendError('Validation Error.', $validator->errors());       
-        // }
-   
-        $input = '$request->all()';
-        // $input['password'] = bcrypt($input['password']);
-        // $user = User::create($input);
-        // $success['token'] =  $user->createToken('MyApp')->accessToken;
-        // $success['name'] =  $user->name;
-   
-        return $this->sendResponse($input, 'subscription products getting successfully.');
+        $subscribe = DB::table('categories')->select()->where('panel', 'subscribe')->get();
+        return $this->sendResponse($subscribe, 'subscription services getting successfully.');
+    }
+
+       public function subscription_products(Request $request)
+    {
+        $lan_child_products = DB::table('products')->select()->where('service_id',$request->service_id)->get();
+        return $this->sendResponse($lan_child_products, 'subscription products getting successfully.');
+    }
+
+       public function subscription_child_products(Request $request)
+    {
+        $lan_child_products = DB::table('child_products')->select()
+            ->where('prod_id',$request->product_id)
+            ->get();
+        return $this->sendResponse($lan_child_products, 'subscription child products getting successfully.');
     }
 
 	    public function user_subscription_products(Request $request)
--- routes/api.php
+++ routes/api.php
@@ -17,7 +17,11 @@ use App\Http\Controllers\API\SubscriptionController;
 Route::post('/register', [RegisterController::class, 'register']);
 Route::post('/login', [RegisterController::class, 'login']);
 Route::get('user-data', [RegisterController::class, 'user_data']);
+
+Route::get('subscription-services', [SubscriptionController::class, 'subscription_services']);
 Route::get('subscription-products', [SubscriptionController::class, 'subscription_products']);
+Route::get('subscription-child-products', [SubscriptionController::class, 'subscription_child_products']);
+
 Route::get('user-subscription-products', [SubscriptionController::class, 'user_subscription_products']);
 Route::post('user-subscription-pro', [SubscriptionController::class, 'user_subscription_pro']);
 Route::post('pause-subscription-products', [SubscriptionController::class, 'pause_subscription_products']);
